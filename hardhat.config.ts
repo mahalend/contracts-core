93d7bb7ba7dd06d69b1fafaa183b208cd61cd80e
Steven Enamakel <enamakel@mahadao.com>
2022-12-12 08:51:56 +0000

    added etherscan verificatino

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -94,6 +94,15 @@ const hardhatConfig: HardhatUserConfig = {
         count: 20,
       },
     },
+    mainnet: {
+      url: 'https://cloudflare-eth.com',
+      accounts: {
+        mnemonic: 'fox sight canyon orphan hotel grow hedgehog build bless august weather swarm',
+        path: "m/44'/60'/0'/0",
+        initialIndex: 0,
+        count: 20,
+      },
+    },
     polygon: {
       url: 'https://polygon-rpc.com/',
       accounts: {
